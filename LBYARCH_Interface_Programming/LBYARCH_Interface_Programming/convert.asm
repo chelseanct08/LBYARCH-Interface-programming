c1a5eb4a97265becf735ad7dc789ade3b1ae6b9b
rae <raek823@gmail.com>
2026-08-07 00:25:17 +0800

    updated asm file

---
+++ LBYARCH_Interface_Programming/LBYARCH_Interface_Programming/convert.asm
@@ -0,0 +1,34 @@
+; convert.asm
+
+section .data
+    var1 dd 255.0
+
+section .text
+bits 64
+default rel
+
+global imgCvtGrayFloatToInt
+
+imgCvtGrayFloatToInt:
+
+    xor r10d, r10d
+    movss xmm1, [var1]
+
+L1:
+
+    cmp r10d, r8d
+    jge L2
+
+    movss xmm0, [rcx + r10*4]
+    mulss xmm0, xmm1
+
+    cvtss2si eax, xmm0 ; cvttss2si
+
+    mov [rdx + r10], al
+    inc r10d
+
+    jmp L1
+
+L2:
+
+    ret
